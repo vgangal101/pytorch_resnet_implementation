1e5b5682046a188e9e15d3e954c570ab004e67e4
Vardaan Gangal <vardaan.gangal@gmail.com>
2022-05-23 05:05:08 +0000

    altered data augmentation

--- cifar10_torchviz_resnet50.py
+++ cifar10_torchviz_resnet50.py
@@ -102,7 +102,6 @@ def load_cifar10_dataset(args):
     # checkout https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py
 
     transform_train = transforms.Compose([
-    transforms.RandomCrop(32,padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
